2a8caeef0b01511b614897f5841febb6c270950e
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-08-31 23:09:08 +0100

    Position hero arrow to the right pointing down toward the hero chart on mobile

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -117,7 +117,7 @@ function Hero() {
             aria-hidden="true"
             width={457}
             height={806}
-            className="pointer-events-none absolute -left-5 bottom-1 w-9 opacity-80 sm:-left-6 sm:bottom-2 sm:w-12 min-[1400px]:-left-24 min-[1400px]:bottom-10 min-[1400px]:w-20 min-[1400px]:opacity-100 block"
+            className="pointer-events-none absolute right-4 bottom-0 w-10 sm:right-8 sm:bottom-2 sm:w-14 min-[1400px]:right-auto min-[1400px]:-left-24 min-[1400px]:bottom-10 min-[1400px]:w-20 block transition-all"
           />
         </Reveal>
 
