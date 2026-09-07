48a1384f8815910fd7106ff8ecab94fa0ad1ef00
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-09-07 13:14:03 +0100

    Fix missing useEffect import in Auth.jsx

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Trans, useTranslation } from "react-i18next";
 import { useQuery } from "@tanstack/react-query";
 import { FcGoogle } from "react-icons/fc";
